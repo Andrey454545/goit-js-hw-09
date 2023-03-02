19d9fd961d9ea0dfed536355c74e704dde91d6b1
Andrey454545 <109917137+Andrey454545@users.noreply.github.com>
2023-03-03 01:28:35 +0200

    update index.html

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -6,7 +6,7 @@ const btnStop = document.querySelector('button[data-stop]');
 btnStop.disabled = true;     // податковий стан кнопки: вимкнена
 let colorInterval = null;    // таймер для зміни кольору
 
-// ф-ція для отримання випадкового кольору
+// ф-ція для отримання(генерації) випадкового кольору
 function getRandomHexColor() {
     return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
