51aa6bb3a0fa35debadfcc12834daceec8154a89
Andrey454545 <109917137+Andrey454545@users.noreply.github.com>
2023-03-05 08:02:42 +0200

    update index.html

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,5 +1,6 @@
 import flatpickr from "flatpickr";  // описаний в документації 
 import "flatpickr/dist/flatpickr.min.css";  // додатковий імпорт стилів
+
 import Notiflix from 'notiflix';
 
 // пошук елементів
